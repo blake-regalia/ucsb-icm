c26e83bb1891b11c999fca0d3f1738d292294f36
Bryan Karaffa <bryankaraffa@gmail.com>
2012-03-02 14:54:27 -0800

    Added toggable sideInfoWindow. Combined CSS stylesheets into 1 theme.css.

---
+++ includes/tools.js
@@ -0,0 +1,9 @@
+//This function is used to display/hide the sideInfoWindow
+//To use, make sure the object has an id set (ex: <div id="foo">)
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'block')
+	  e.style.display = 'none';
+   else
+	  e.style.display = 'block';
+}
