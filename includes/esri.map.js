dac3b06ab67755fe7cf73466812438262bf39a7d
Bryan Karaffa <bryankaraffa@gmail.com>
2012-03-02 18:05:13 -0800

    Changed default extent to match the original ICM. Made slight changes to html and css to make layout look more like the original ICM.

--- includes/esri.map.js
+++ includes/esri.map.js
@@ -31,8 +31,14 @@ window.DefaultPackage = UCSB_Campus;
 		//Settings for the position and size of the Zoom slider.
 		//esriConfig.defaults.map.slider = { left:"10px", top:"10px", width:null, height:"200px" };
 		
+		//openingExtent is the extent to zoom to UCSB Campus.
+		openingExtent = new esri.geometry.Extent({"xmin":-119.86100,"ymin":34.40856,"xmax":-119.83553,"ymax":34.41913,"spatialReference":{"wkid":4326}});
+		
 		map = new esri.Map('map', {
-			extent: package.extent,
+			//package.extent will use the extent from the layer loaded (The basemap)
+			//extent: package.extent,
+			//Instead, we want to set the extent to UCSB campus.
+			extent: openingExtent,
 		});
 		
 		// Add Basemap
