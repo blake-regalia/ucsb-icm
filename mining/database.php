be8842990f7cf527f402e4f623a1bfec1a071f4c
Blake Regalia <blake.regalia@gmail.com>
2012-05-02 23:21:22 -0700

    registrar scraper

---
+++ mining/database.php
@@ -0,0 +1,161 @@
+<?php
+
+if(!isset($DATABASE)) {
+	require("database.config.php");
+}
+
+
+class MySQL_Pointer {
+	
+	static private $link = NULL;
+	static private $user = 'root';
+	static private $pass = '';
+	
+	protected $database = FALSE;
+	protected $table = FALSE;
+	
+	
+	protected function query($sql) {
+		$resource = mysql_query($sql, self::$link);
+		return $resource;
+	}
+	
+	protected function attempt($sql) {
+		if($this->database !== FALSE) {
+			$select = mysql_select_db($this->database, self::$link);
+			if($select !== TRUE) {
+				$this->error('could not select database '.$this->database);
+			}
+		}
+		
+		$attempt = $this->query($sql);
+		
+		if($attempt === TRUE) {
+			return TRUE;
+		}
+		else if($attempt === FALSE) {
+			return FALSE;
+		}
+		else {
+			$this->error('incorrect usage of function: attempt(); result not boolean');
+		}
+	}
+	
+	function __construct($database) {
+		if($this->attempt("CREATE DATABASE IF NOT EXISTS `".$database."`") === FALSE) {
+			$this->error('failed to create database: '.$sql.'; ');
+			exit;
+		}
+		$this->database = $database;
+	}
+	
+	function truncate($table) {
+		$sql = "TRUNCATE TABLE `".$this->database."`.`".$table."`";
+		return $this->attempt($sql);
+	}
+	
+	function dropTable($table) {
+		$sql = "DROP TABLE `".$this->database."`.`".$table."`";
+		return $this->attempt($sql);
+	}
+	
+	function fetchAssoc() {
+		$sql = "SELECT * FROM ".$this->path.";";
+		$res = $this->query($sql);
+		while(($resultArray[] = mysql_fetch_assoc($res)) || array_pop($resultArray));
+		return $resultArray;
+	}
+	
+	function fetchAssocOf($field) {
+		$sql = "SELECT * FROM ".$this->path.";";
+		$res = $this->query($sql);
+		
+		while(true) {
+			$result = mysql_fetch_assoc($res);
+			if(!$result) break;
+			$resultArray[] = $result[$field];
+		}
+		return $resultArray;
+	}
+	
+	function fetchSizeWhereEquals($field, $value) {
+		$sql = "SELECT COUNT(*) FROM ".$this->path." WHERE `".$field."`='".$value."';";
+		return mysql_result($this->query($sql), 0);
+	}
+	
+	function fetchRow($field, $value) {
+		$sql = "SELECT * FROM ".$this->path." WHERE `".$field."` = '".$value."';";
+		$res = $this->query($sql);
+		return mysql_fetch_assoc($res);
+	}
+	
+	function fetchAssocWhereEquals($field, $value) {
+		$sql = "SELECT * FROM ".$this->path." WHERE `".$field."` = '".$value."';";
+		$res = $this->query($sql);
+		while(($resultArray[] = mysql_fetch_assoc($res)) || array_pop($resultArray));
+		return $resultArray;
+	}
+	
+	function insert($array) {
+		$fields = implode('`,`', array_keys($array));
+		$values = implode("','", array_values($array));
+		$sql = "INSERT INTO ".$this->path." (`".$fields."`) VALUES('".$values."');";
+		return $this->query($sql);
+	}
+	
+	function selectTable($table) {
+		$this->table = $table;
+		$this->path = "`".$this->database."`.`".$table."`";
+	}
+	
+	function table($tableName, $fieldArray) {
+		$fields = array();
+		foreach($fieldArray as $name => $type) {
+			$fields []= '`'.$name.'` '.$type;
+		}
+		$sql = "CREATE TABLE IF NOT EXISTS `".$tableName."` (".implode(',', $fields).");";
+		return $this->attempt($sql);
+	}
+	
+	protected function error($say) {
+		echo $say;
+		echo "\n".mysql_error();
+		exit;
+	}
+	
+	static function init($server, $user, $pass, $new, $flags) {
+		self::$user = $user;
+		self::$pass = $pass;
+		
+		$link = mysql_connect($server, $user, $pass, $new, $flags);
+		
+		if($link === FALSE) {
+			die('failed to connect to MySQL server');
+			exit;
+		}
+		else {
+			self::$link = $link;
+		}
+	}
+	
+}
+
+MySQL_Pointer::init(
+	// server host name
+	$DATABASE['HOST'],
+	
+	// user name
+	$DATABASE['USER'],
+	
+	// password
+	$DATABASE['PASS'],
+	
+	// new link
+	false,
+	
+	// client flags
+	0
+);
+
+
+?>
--- mining/registrar.php
+++ mining/registrar.php
@@ -1,6 +1,19 @@
 <?php
 
 require "phpQuery.php";
+require "database.php";
+
+
+
+$limitOneDepartment = false;
+
+$param_qtr = '20122'; // spring 2012
+
+$db_table_name = 'spring-2012-ugrad';
+
+$param_level = 'Undergraduate';
+// or uncomment the line below to mine for graduate courses
+//$param_level = 'Graduate';
 
 $regUrl = "http://my.sa.ucsb.edu/public/curriculum/coursesearch.aspx";
 
@@ -49,65 +62,69 @@ $ASP_Tokens = array(
 	'__EVENTVALIDATION' => pq('#__EVENTVALIDATION')->val(),
 );
 
+// store all the department options
+$courseOptions = array();
+
+function saveCourseOption($option) {
+	// php bug won't allow call by reference in user-defined functions, use global instead
+	global $courseOptions;
+	$courseOptions[] = pq($option)->val();
+}
+
+// fetch all of the course list option values
+pq('select[id$="courseList"]')->find('option')->each('saveCourseOption', new CallbackParam);
+
 // cleanup
 unset($initPage);
 
 
 
-
-/* perform a departmental request */
-
-// initialize a curl object
-$regHandle = curl_init();
-
-// prepare the post field data
-$regPostFieldsArray = $ASP_Tokens + array(
-	'ctl00$pageContent$courseList'           => 'ANTH    ',
-	'ctl00$pageContent$quarterList'          => '20122',
-	'ctl00$pageContent$dropDownCourseLevels' => 'Undergraduate',
-	'ctl00$pageContent$searchButton.x'       => '22',
-	'ctl00$pageContent$searchButton.y'       => '8'
-);
-
-// encode the post fields as a url component
-$regPostFieldsArrayEncoded = array();
-foreach($regPostFieldsArray as $key => $value) {
-	$regPostFieldsArrayEncoded[] = urlencode($key).'='.urlencode($value);
+if($limitOneDepartment) {
+	$courseOptions = array($param_dept);
 }
-$regPostFieldsString = implode('&', $regPostFieldsArrayEncoded);
 
-// extend any additional curl options to the default ones
-$regOpts = $curlOpts + array(
-	CURLOPT_POST => true,
-	CURLOPT_POSTFIELDS => $regPostFieldsString,
-	
-	CURLOPT_COOKIE => implode(';', $regCookie),
-);
-curl_setopt_array($regHandle, $regOpts);
 
-// perform get request
-$regHtml = curl_exec($regHandle);
-curl_close($regHandle);
-unset($regHandle);
+$db = new MySQL_Pointer($DATABASE['db_name']);
 
-// parse the document
-$doc = phpQuery::newDocument($regHtml);
-phpQuery::selectDocument($doc);
+$db->table($db_table_name, array(
+	'courseTitle'    => 'varchar(255)',
+	'fullTitle'      => 'varchar(255)',
+	'description'    => 'varchar(255)',
+	'preReq'         => 'varchar(255)',
+	'college'        => 'varchar(255)',
+	'units'          => 'varchar(255)',
+	'grading'        => 'varchar(255)',
+	'primaryTitle'   => 'varchar(255)',
+	'status'         => 'varchar(255)',
+	'enrollCode'     => 'varchar(255)',
+	'levelLimit'     => 'varchar(255)',
+	'majorLimitPass' => 'varchar(255)',
+	'majorLimit'     => 'varchar(255)',
+	'messages'       => 'varchar(255)',
+	'instructor'     => 'varchar(255)',
+	'days'           => 'varchar(255)',
+	'time'           => 'varchar(255)',
+	'location'       => 'varchar(255)',
+	'enrolled'       => 'varchar(255)',
+));
+
+$db->selectTable($db_table_name);
+
+
 
 function error($msg) {
 	die($msg);
 }
 
-$records = array();
-
-
 function format($str) {
 	return preg_replace(array('/^\s+/','/\s+$/'),array('',''),$str);
 }
 
+$records = array();
+
 /** function responsible for extracting fields from each row **/
 function extractRow($row) {
-	global $records;
+	global $records, $db;
 	$array = &$records;
 	
 	// setup a reference to the child TD elements
@@ -160,7 +177,7 @@ function extractRow($row) {
 	// time of day codes
 	$enrolled = format($tds->eq(9)->text());
 	
-	$array[] = array(
+	$insert = array(
 		'courseTitle'    => $courseTitle,
 		'fullTitle'      => $fullTitle,
 		'description'    => $description,
@@ -181,15 +198,64 @@ function extractRow($row) {
 		'location'       => $location,
 		'enrolled'       => $enrolled,
 	);
+	
+	$db->insert($insert);
+	$array[] = $insert;
 }
 
-pq('.CourseInfoRow')->each('extractRow',new CallbackParam);
 
-// cleanup
-unset($regPage);
+ob_start();
 
-
-print_r($records);
+/* iterate through every department and scrape their info */
+foreach($courseOptions as $department) {
+	
+	// initialize a curl object
+	$regHandle = curl_init();
+	
+	// prepare the post field data
+	$regPostFieldsArray = $ASP_Tokens + array(
+		'ctl00$pageContent$courseList'           => $department,
+		'ctl00$pageContent$quarterList'          => $param_qtr,
+		'ctl00$pageContent$dropDownCourseLevels' => $param_level,
+		'ctl00$pageContent$searchButton.x'       => '22',
+		'ctl00$pageContent$searchButton.y'       => '8'
+	);
+	
+	
+	// encode the post fields as a url component
+	$regPostFieldsArrayEncoded = array();
+	foreach($regPostFieldsArray as $key => $value) {
+		$regPostFieldsArrayEncoded[] = urlencode($key).'='.urlencode($value);
+	}
+	$regPostFieldsString = implode('&', $regPostFieldsArrayEncoded);
+	
+	// extend any additional curl options to the default ones
+	$regOpts = $curlOpts + array(
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => $regPostFieldsString,
+		
+		CURLOPT_COOKIE => implode(';', $regCookie),
+	);
+	curl_setopt_array($regHandle, $regOpts);
+	
+	// perform get request
+	$regHtml = curl_exec($regHandle);
+	curl_close($regHandle);
+	unset($regHandle);
+	
+	// parse the document
+	$doc = phpQuery::newDocument($regHtml);
+	phpQuery::selectDocument($doc);
+	
+	pq('.CourseInfoRow')->each('extractRow',new CallbackParam);
+	
+	// cleanup
+	unset($regPage);
+	
+	echo $department."\n";
+	ob_flush();
+	flush();
+}
 
 
 
