333e1f1f287c07e9c17ee997fb49e35ff70f6c24
Blake Regalia <blake.regalia@gmail.com>
2012-08-24 23:17:30 -0700

    deleted old files

--- dev/read.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$data = file_get_contents('winter.2012.txt');
-
-echo preg_replace('~%([0-9a-f]{2})~ei', 'chr(hexdec("\\1"))', $data);
-?>
--- sandbox/buildings/save.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$name = $_POST['name'];
-$data = $_POST['data'];
-
-file_put_contents($name, $data);
-
-?>
